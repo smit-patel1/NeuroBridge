437365943048a1b32b4f91393fc0cbec91f646b5
Smit Patel <64547898+smit-patel1@users.noreply.github.com>
2025-06-01 16:28:06 -0400

    Updated vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: 'frontend', // tell Vite to start in the frontend folder
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'frontend/src'),
     },
   },
-})
+  build: {
+    outDir: '../dist', // output build to project root /dist
+  },
+});
